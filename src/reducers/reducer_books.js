552b8e67e7dece59ff36a127dc2ea55b05570bcc
lakhanrochwani <rochwanilakhan@gmail.com>
2018-09-02 20:05:17 -0500

    First commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+      {title:'HTML and CSS revisited',author:'Bucky Roberts'},
+      {title:'Front End developer in 2k18',author:'Tenzin Phuljung'},
+      {title:'ReactJS for begineers',author:'Stephen Grider'},
+      {title:'Grunt and Gulp crash course',author:'Andrew Mead'}
+  ];
+}
